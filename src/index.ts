c985a2b5dfbfdb6b75165a9e6986fa98d320389d
productionbug <nghosh@uoguelph.ca>
2022-09-17 15:45:21 -0400

    text to speech working

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,41 @@ import indexRouter from "./routes/healthCheck";
 import parleScore from "./routes/parleScore";
 import cors from "cors";
 
+import speech from "@google-cloud/speech";
+import fs from "fs";
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+async function main() {
+  // The path to the remote LINEAR16 file
+  const gcsUri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    uri: gcsUri,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  };
+  const request: any = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file
+  const [response]: any = await client.recognize(request);
+  const transcription = response.results
+    .map((result: any) => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
+}
+
+(async () => {
+  await main();
+})()
 
 const app = express();
 
