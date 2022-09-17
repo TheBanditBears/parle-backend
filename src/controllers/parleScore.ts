626e04e8fc09038f3310a3e66d4403b9e897fac5
productionbug <nghosh@uoguelph.ca>
2022-09-17 12:03:30 -0400

    cohere

--- src/controllers/parleScore.ts
+++ src/controllers/parleScore.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
+import runCohere from "../services/parleScore"; 
 
-export default function parleScore(req: Request, res: Response, next: NextFunction) {
+export default async function parleScore(req: Request, res: Response, next: NextFunction) {
+    await runCohere();
     res.status(200).json({ parleScore: 100 });
 }
--- src/routes/parleScore.ts
+++ src/routes/parleScore.ts
@@ -4,6 +4,8 @@ import parleScoreController from "../controllers/parleScore";
 const router = express.Router();
 
 /* GET home page. */
-router.post('/parle-score', parleScoreController);
+router.post("/parle-score", async (req, res, next) => {
+  await parleScoreController(req, res, next);
+});
 
 export default router;
